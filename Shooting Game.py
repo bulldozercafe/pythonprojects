73affc27f7ed6c6162fbadfb5735690ccd8c78c1
bulldozercafe <zeldason12@gmail.com>
2024-11-03 15:04:32 +0900

    수정

--- Shooting Game.py
+++ Shooting Game.py
@@ -37,7 +37,7 @@ def enemies_list(enemies, frame):
                 x_pos = 850 # <--                                   0      1      2      3       4
                 x_vel = -10
                 y_vel = 0
-            enemies.append([x_pos, 200, x_vel,y_vel , 100])    # [x_pos, y_pos, x_vel, y_vel,  health]
+            enemies.append([x_pos, 200, x_vel,y_vel , 3])    # [x_pos, y_pos, x_vel, y_vel,  health]
 
 
 def gunshots(x_pos, y_pos, bullet, frame):
@@ -53,20 +53,21 @@ while play:
         if event.type == pygame.QUIT:  
             play = False
 
-        # 키로 동작
-        if event.type == pygame.KEYDOWN:  
-            if event.key in (pygame.K_RIGHT, pygame.K_d):
-                left = False
-                right = True
-                to_x = 10
-            elif event.key in (pygame.K_LEFT, pygame.K_a):
-                right = False
-                left = True
-                to_x = -10
-        else:
-            right = False
+    # 키로 동작
+    if event.type == pygame.KEYDOWN:  
+        if event.key in (pygame.K_RIGHT, pygame.K_d):
             left = False
-            to_x = 0
+            right = True
+            to_x = 10
+        elif event.key in (pygame.K_LEFT, pygame.K_a):
+            right = False
+            left = True
+            to_x = -10
+    else:
+        right = False
+        left = False
+        to_x = 0
+
     
     x_pos += to_x
     if x_pos < 0:  # 왼쪽 경계
@@ -77,31 +78,33 @@ while play:
 
 
 
+    # ===============마우스 위치 가져오기 ================
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    x_pos = mouse_x
+    # ===================================================
+
+    
+
+
+
+    # 화면에 그리기 시작
     background.fill('blue')
 
-     # 총알 생성
+
+    # 총알 생성
     gunshots(x_pos, y_pos, bullet, frame)
+    
     # 총알 그리기
     for gun in bullet:
         gun[1] -= 20
         pygame.draw.rect(background, ('yellow'), (gun[0], gun[1], 10, 20))
 
 
-    # 적이랑 총알이 만났는지 확인하고 삭제하기
-    #for e in enemies:
-    #    for b in bullet:
-            
-
-
-
     # 플레이어 그리기
     pygame.draw.rect(background, ('white'), (x_pos, y_pos, 50, 50))
 
 
-
-
-
-    # 적 생성
+    # 적 새로 만들기
     enemies_list(enemies, frame)
 
     # 적 그리기
@@ -122,6 +125,44 @@ while play:
         en[1] += en[3]
 
         pygame.draw.rect(background, ('red'), (en[0], en[1], 50, 50))
+
+        # ========================== 적 체력 표시하기 ====================
+        # 폰트 설정 (폰트 이름, 크기)
+        font = pygame.font.Font(None, 36)  # 기본 폰트 사용, 크기 36
+        text = font.render(str(en[4]), True, (255, 255, 255))  # 흰색 글씨
+        background.blit(text, (en[0], en[1], 50, 50))
+        # ================================================================
+
+    
+
+    # ===========적이랑 총알이 만났는지 확인하고 삭제하기================
+    for e in enemies:
+        for b in bullet:
+            enemy_rect = pygame.Rect(e[0], e[1], 50, 50)  # 적 사각형 데이터 만들기
+            bullet_rect = pygame.Rect(b[0], b[1], 10, 20)     # 총알 사각형 데이터 만들기
+
+            # 충돌 확인
+            if bullet_rect.colliderect(enemy_rect):   # 서로 겹쳤는지 검사 -> 겹쳤으면 둘다 삭제
+                pygame.draw.rect(background, ('green'), bullet_rect, 5) #반짝 효과
+                pygame.draw.rect(background, ('green'), enemy_rect, 5)  #반짝 효과
+                bullet.remove(b)
+                e[4] -= 1  # 적 체력 1 깎기
+                if(e[4] < 1): # 적 체력이 1보다 작으면 삭제하기
+                    enemies.remove(e)
+    # =================================================================
+
+    # ===========적이랑 플레이어랑 만났는지 확인하기  ================
+    player_rect = pygame.Rect(x_pos, y_pos, 50, 50)
+    for e in enemies:
+        enemy_rect = pygame.Rect(e[0], e[1], 50, 50)  # 적 사각형 데이터 만들기
+        
+        # 충돌 확인
+        if player_rect.colliderect(enemy_rect):   # 서로 겹쳤는지 검사 -> 겹쳤으면 처리
+            pygame.draw.rect(background, ('green'), enemy_rect, 5)  #반짝효과
+            pygame.draw.rect(background, ('green'), player_rect, 5)  #반짝효과
+            print("으악!!")
+    # =================================================================
+            
 
     
     
